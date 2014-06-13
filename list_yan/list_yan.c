8df01e0c84b223f9e5755f51de3e1d99cd301461
qeesung <qeesung@qq.com>
2014-06-13 21:06:43 +0800

    change the files

--- list_yan/list_yan.c
+++ list_yan/list_yan.c
@@ -1,14 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"./list_yan.h"
+#ifndef _LIST_YAN_H
+#define _LIST_YAN_H
 
-struct Listnode
+struct Listnode;
+typedef struct Listnode * List;
+
+/* create a list  */
+List Create_List(int number)
 {
-	int number;
-	char * name;
-	List next;
-};
+	List new_list;
+	List temp;
+	int i;
+	// create the header 
+	new_list=malloc(sizeof(struct Listnode));
+	if(new_list == NULL)
+	{
+		printf("out of space create the haeder failed\n");
+		return NULL;
+	}
+	// and now create the body
+	temp=new_list;
+	for(i=0;i<number;i++)
+	{
+		temp->next=malloc(sizeof(struct Listnode));
+		if(temp->next==NULL)
+		{
+			printf("create the Listnode failed\n");
+			Delete_List(new_list);// delete has been malloc
+			return NULL;
+		}
+		printf("enter #%d number:",i);
+		scanf("%d",&(temp->next->number));
+		printf("\n");
+		printf("enter #%d name:",i);
+		scanf("%s",temp->next->name);
+		temp=temp->next;// insert done
+		
+	}
+	return new_list;
+}
 
-
-/* create a list */
-List Create_List(int number);
+#endif
--- list_yan/list_yan.h
+++ list_yan/list_yan.h
@@ -1,42 +1,18 @@
-#ifndef _LIST_YAN_H
-#define _LIST_YAN_H
 
-struct Listnode;
-typedef struct Listnode * List;
+#include<stdio.h>
+#include<stdlib.h>
+#include"./list_yan.h"
 
-/* create a list  */
-List Create_List(int number)
+struct Listnode
 {
-	List new_list;
-	List temp;
-	int i;
-	// create the header 
-	new_list=malloc(sizeof(struct Listnode));
-	if(new_list == NULL)
-	{
-		printf("out of space create the haeder failed\n");
-		return NULL;
-	}
-	// and now create the body
-	temp=new_list;
-	for(i=0;i<number;i++)
-	{
-		temp->next=malloc(sizeof(struct Listnode));
-		if(temp->next==NULL)
-		{
-			printf("create the Listnode failed\n");
-			Delete_List(new_list);// delete has been malloc
-			return NULL;
-		}
-		printf("enter #%d number:",i);
-		scanf("%d",&(temp->next->number));
-		printf("\n");
-		printf("enter #%d name:",i);
-		scanf("%s",temp->next->name);
-		temp=temp->next;// insert done
-		
-	}
-	return new_list;
-}
+	int number;
+	char * name;
+	List next;
+};
 
-#endif
+
+/* create a list */
+List Create_List(int number);
+
+
+
