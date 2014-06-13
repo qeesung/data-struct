4342c6dcacb934563cd0367762cc7660dc8c6127
qeesung <qeesung@qq.com>
2014-06-13 21:23:19 +0800

    add function Get_Node()

--- list_yan/list_yan.c
+++ list_yan/list_yan.c
@@ -3,40 +3,9 @@
 
 struct Listnode;
 typedef struct Listnode * List;
-
-/* create a list  */
-List Create_List(int number)
-{
-	List new_list;
-	List temp;
-	int i;
-	// create the header 
-	new_list=malloc(sizeof(struct Listnode));
-	if(new_list == NULL)
-	{
-		printf("out of space create the haeder failed\n");
-		return NULL;
-	}
-	// and now create the body
-	temp=new_list;
-	for(i=0;i<number;i++)
-	{
-		temp->next=malloc(sizeof(struct Listnode));
-		if(temp->next==NULL)
-		{
-			printf("create the Listnode failed\n");
-			Delete_List(new_list);// delete has been malloc
-			return NULL;
-		}
-		printf("enter #%d number:",i);
-		scanf("%d",&(temp->next->number));
-		printf("\n");
-		printf("enter #%d name:",i);
-		scanf("%s",temp->next->name);
-		temp=temp->next;// insert done
-		
-	}
-	return new_list;
-}
-
+typedef List Position;
+/* Create a list */
+List Create_List(int number);
+/* get the #i number */
+Position Get_Node(List my_list,int i);
 #endif
--- list_yan/list_yan.h
+++ list_yan/list_yan.h
@@ -1,4 +1,3 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 #include"./list_yan.h"
@@ -9,10 +8,63 @@ struct Listnode
 	char * name;
 	List next;
 };
-
-
-/* create a list */
-List Create_List(int number);
-
+List Create_List(int numebr)
+{
+	List new_list;
+	List temp;
+	int i;
+	// create the header 
+	new_list=malloc(sizeof(struct Listnode));
+	if(new_list == NULL)
+	{
+		printf("out of space create the haeder failed\n");
+		return NULL;
+	}
+	// and now create the body
+	temp=new_list;
+	for(i=0;i<number;i++)
+	{
+		temp->next=malloc(sizeof(struct Listnode));
+		if(temp->next==NULL)
+		{
+			printf("create the Listnode failed\n");
+			Delete_List(new_list);// delete has been malloc
+			return NULL;
+		}
+		printf("enter #%d number:",i);
+		scanf("%d",&(temp->next->number));
+		printf("\n");
+		printf("enter #%d name:",i);
+		scanf("%s",temp->next->name);
+		temp=temp->next;// insert done
+		
+	}
+	return new_list;
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Position Get_Node(List my_list , int i,int list_size)
+{
+	Position temp;
+	int i;
+	if(my_list->next==NULL)
+	{
+		printf("the List is empty\n");
+		return NULL;
+	}
+	if(i>list_size)
+	{
+		printf("can not find the element\n");
+		return NULL;
+	}
+	temp=my_list;
+	for(i=0;i<i;i++)
+	{	
+		temp=temp->next;
+		
+	}
+	return temp;
+}
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#endif
 
 
