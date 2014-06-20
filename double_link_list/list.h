eca60b3812105b732d23ea69d36ed8e18e4dbbe8
qeesung <qeesung@qq.com>
2014-06-20 23:25:26 +0800

    define the list.h about double link list

---
+++ double_link_list/list.h
@@ -0,0 +1,69 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  list.h
+ *
+ *    Description:  This header file is for double link list
+ *
+ *        Version:  1.0
+ *        Created:  2014年06月20日 23时15分06秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  qeesung (), 1245712564@qq.com
+ *   Organization:  STAY HUNGRY STAY FOOLISH
+ *
+ * =====================================================================================
+ */
+
+#ifndef _LIST_H
+#define _LIST_H
+
+struct Listnode {
+	int number ;
+	struct Listnode * next;/*  point to next element */
+	struct Listnode * prior;/*  ponter to last element */
+};				/* ----------  end of struct Listnode  ---------- */
+
+typedef struct Listnode * List;
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Create_List
+ *  Description:  create a empty list
+ * =====================================================================================
+ */
+	List
+Create_List ();
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Insert_List
+ *  Description:  insert a element into the list
+ * =====================================================================================
+ */
+	void
+Insert_List (List list , int number ); 
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Delete_List
+ *  Description:  delete a element form the list
+ * =====================================================================================
+ */
+	void
+Delete_List (List list , int number); 
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Print_List
+ *  Description:  print all the element in the list
+ * =====================================================================================
+ */
+	void
+Print_List ( List list);
+
+
+#endif
