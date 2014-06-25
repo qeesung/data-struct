721750e27d2eec4acc16d9fe13439b93e88e92b3
qeesung <qeesung@qq.com>
2014-06-26 04:42:19 +0800

    add string.h about dynamic

---
+++ String/dynamic_string/string.h
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  string.h
+ *
+ *    Description:  dynamic string header file
+ *
+ *        Version:  1.0
+ *        Created:  2014年06月26日 04时33分10秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  qeesung (), 1245712564@qq.com
+ *   Organization:  STAY HUNGRY STAY FOOLISH
+ *
+ * =====================================================================================
+ */
+
+#ifndef _STRING_H
+#define _STRING_H
+
+struct string {
+	char * ch;
+	int length;
+};				/* ----------  end of struct string  ---------- */
+
+typedef struct string  * String;
+
+String Str_Assign(String string);// copy a string
+int Str_Length(String string);
+int Str_Compare(String string1 , String string2);
+void Clear_String(String string);
+String Concat(String string1 , String string2);
+String Sub_String(String string , int pos , int len);
+void Str_Insert(String string , int pos , String new_string);// insert new_string into string  at pos
+
+
+#endif
