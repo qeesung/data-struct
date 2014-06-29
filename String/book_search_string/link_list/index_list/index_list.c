ea66e7ba223b0166d668a982bb9aa92dc73891c5
qeesung <qeesung@qq.com>
2014-06-30 05:44:19 +0800

    add Insert_Index_List() function

--- String/book_search_string/link_list/index_list/index_list.c
+++ String/book_search_string/link_list/index_list/index_list.c
@@ -41,3 +41,39 @@ Init_Index_List ( )
 	
 	return ;
 }		/* -----  end of function Init_Index_List  ----- */
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Insert_Index_List
+ *  Description:  insert a index to the index list
+ * =====================================================================================
+ */
+	void
+Insert_Index_List ( Index_list list, int index )
+{
+	Index_position temp;
+	Index_position new_index_node;
+	if(list==NULL)
+	{
+		fprintf(stderr,"\n the list is not init\n ");
+		return ;
+	}
+
+	new_index_node	= malloc ( sizeof(index_node) );
+	if ( new_index_node==NULL ) {
+		fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+		exit (EXIT_FAILURE);
+	}
+	new_index_node->index_number = index;
+	while(list->next!=NULL && list->next->index_number < index)
+	{
+		list=list->next;
+	}
+	temp=list->next;
+	list->next=new_index_node;
+	list->next->next=temp;// insert done
+	
+	return;
+}		/* -----  end of function Insert_Index_List  ----- */
