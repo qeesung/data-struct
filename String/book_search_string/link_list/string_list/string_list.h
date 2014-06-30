224bee2903de49b6e52504265142e48e59cd14a2
qeesung <qeesung@qq.com>
2014-06-30 21:44:40 +0800

    add string_list header file

---
+++ String/book_search_string/link_list/string_list/string_list.h
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  string_list.h
+ *
+ *    Description:  this is string link list haeder file
+ *
+ *        Version:  1.0
+ *        Created:  2014年06月30日 21时36分44秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  qeesung (), 1245712564@qq.com
+ *   Organization:  STAY HUNGRY STAY FOOLISH
+ *
+ * =====================================================================================
+ */
+
+#ifndef _STRING_LIST_H
+#define _STRING_LIST_H
+#include"./index_list/index_list.h"
+
+
+struct string_node {
+	char * book_word;
+	Index_list book_index;
+	struct string_node * next;
+};				/* ----------  end of struct string_node  ---------- */
+
+typedef struct string_node  * String_node;
+typedef String_node String_list;
+typedef String_list String_position;
+
+String_list Init_String_List();
+void Insert_String_List(String_list list,char * word , int index_number );
+void Delete_String_List(String_list list , char * word, int index_number );
+void Print_String_list(String_list list);
+String_position Find_Position(String_list list , char * word);
+void Write_String_List(char * filename, String_list list);
+
+
+
+#endif
