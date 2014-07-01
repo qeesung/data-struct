0f649955038ddc5c0fdb444ab6ceee1409becfc3
qeesung <qeesung@qq.com>
2014-07-02 04:11:36 +0800

    add matrix header file

---
+++ Matrix/matrix.h
@@ -0,0 +1,48 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  matrix.h
+ *
+ *    Description:  this is matrix  header file
+ *
+ *        Version:  1.0
+ *        Created:  2014年07月02日 04时02分23秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  qeesung (), 1245712564@qq.com
+ *   Organization:  STAY HUNGRY STAY FOOLISH
+ *
+ * =====================================================================================
+ */
+
+#ifndef _MATRIX_H
+#define _MATRIX_H
+#define MATRIX_NODE_NUMBER 100
+
+struct matrix_node {
+	int i;
+	int j;
+	int number ;
+};				/* ----------  end of struct matrix_node  ---------- */
+
+
+struct matrix {
+	struct matrix_node [MATRIX_NODE_NUMBER]
+	int mu;
+	int nu;
+	int tu;
+};				/* ----------  end of struct matrix  ---------- */
+
+typedef struct matrix *  Matrix;
+
+Matrix Create_Matrix();
+void Destory_Matrix();
+void Insert_Matrix(int mu , int nu, int number );
+void Delete_Matrix(int mu , int nu);
+Matrix Add_Matrix(Matrix my_matrix1 , Matrix my_matrix2);
+Matrix Sub_Matrix(Matrix my_matrix1 , Matrix my_matrix2);
+Matrix Mul_Matrix(Matrix my_matrix1 , Matrix my_matrix2);
+Matrix Transpose_Matrix(Matrix my_matrix);
+
+#endif
