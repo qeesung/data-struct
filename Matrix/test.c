03147374a56774e4df2152432c5dcd3813b139ca
qeesung <qeesung@qq.com>
2014-07-02 23:19:42 +0800

    add test file and elf binary file

--- Matrix/test.c
+++ Matrix/test.c
@@ -31,24 +31,45 @@ main ( int argc, char *argv[] )
 {
 	int i;
 	Matrix my_matrix ;
+	Matrix trans_matrix;
+	Matrix add_matrix;
+	Matrix sub_matrix;
+	Matrix mul_matrix;
 	my_matrix=Create_Matrix(5,5);
-	Insert_Matrix(my_matrix,2,2,4);
-	Insert_Matrix(my_matrix,4,4,1);
-	Insert_Matrix(my_matrix,2,3,14);
+	Insert_Matrix(my_matrix,2,5,4);
+	Insert_Matrix(my_matrix,4,1,1);
+	Insert_Matrix(my_matrix,3,2,8);
 	Insert_Matrix(my_matrix,3,3,3);
-	Insert_Matrix(my_matrix,5,5,7);
+	Insert_Matrix(my_matrix,5,2,7);
 	Insert_Matrix(my_matrix,1,1,5);
 	Print_Matrix(my_matrix);
-	for(i=0;i<my_matrix->tu;i++)
-	{
-		printf("%d \t ",my_matrix->data[i].number);
-	}
-	printf("\n ++++++++++++++++++++\n ");
-	Delete_Matrix(my_matrix,2,3);
+	printf("\n ++++++++++after delete++++++++++\n ");
+	Delete_Matrix(my_matrix,3,3);
 	Print_Matrix(my_matrix);
-	for(i=0;i<my_matrix->tu;i++)
+	/*for(i=0;i<my_matrix->tu;i++)
 	{
 		printf("%d \t ",my_matrix->data[i].number);
-	}
+	}*/
+	trans_matrix=Transpose_Matrix(my_matrix);
+	printf("\n ++++++++++after transpose++++++++++\n ");
+	Print_Matrix(trans_matrix);
+	add_matrix=Add_Matrix(my_matrix,trans_matrix);
+	printf("\n ++++++++++after add++++++++++\n ");
+	Print_Matrix(add_matrix);
+	sub_matrix=Sub_Matrix(my_matrix, trans_matrix);
+	printf("\n ++++++++++after sub++++++++++\n ");
+	Print_Matrix(sub_matrix);
+	printf("\n ++++++++++after mul++++++++++\n ");
+	mul_matrix=Mul_Matrix(my_matrix, trans_matrix);
+	Print_Matrix(mul_matrix);
+	/*  destrory all the matrix */
+	Destory_Matrix(my_matrix);
+	Destory_Matrix(sub_matrix);
+	Destory_Matrix(add_matrix);
+	Destory_Matrix(trans_matrix);
+	Destory_Matrix(mul_matrix);
+
+
+
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
