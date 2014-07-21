d765a058d94f3f5fe73dc6507fa3153ed3d3ea46
qeesung <1245712564@qq.com>
2014-07-22 01:17:43 +0800

    add Init_Tree() function

---
+++ Tree/link_binary_tree/test/test.c
@@ -0,0 +1,61 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  test for huawei mianshi
+ *
+ *        Version:  1.0
+ *        Created:  2014年07月22日 01时13分06秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  qeesung (), 1245712564@qq.com
+ *   Organization:  Stay hangry , Stay foolish
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include    <stdio.h>
+
+#define MAX_TREE_SIZE 50
+#define None -1
+typedef int * Tree;
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Init_Tree
+ *  Description:  int my binary tree
+ * =====================================================================================
+ */
+    Tree
+Init_Tree ( )
+{
+    Tree new_tree;
+    int i=0;
+    new_tree	= malloc ( sizeof(int) * MAX_TREE_SIZE );
+    if ( new_tree==NULL ) {
+        fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+        exit (EXIT_FAILURE);
+    }
+
+    for(i=0;i<MAX_TREE_SIZE ; i++)
+    {
+       new_tree[i]=None;
+    }
+
+    return new_tree;
+}		/* -----  end of function Init_Tree  ----- */
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+    int
+main ( int argc, char *argv[] )
+
+    return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
