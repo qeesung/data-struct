71bd78ca063dc33a2e43480d97a0ea20ec4dc36d
qeesung <1245712564@qq.com>
2014-07-23 06:37:31 +0800

    add queue header file

---
+++ Tree/link_binary_tree/queue.h
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  queue.h
+ *
+ *    Description:  This is queue header file
+ *
+ *        Version:  1.0
+ *        Created:  2014年07月23日 06时29分24秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  qeesung (), 1245712564@qq.com
+ *   Organization:  Stay hangry , Stay foolish
+ *
+ * =====================================================================================
+ */
+
+#ifndef _QUEUE_H
+#define _QUEUE_H
+#include"binary_tree.h"
+struct list
+{
+    struct tree tree_node;
+    struct list * next ;
+
+};
+
+typedef struct list * List;
+struct queue
+{
+    List queue_list;
+    List front ;
+    List rear;
+};
+typedef struct queue * Queue;
+
+
+#endif
