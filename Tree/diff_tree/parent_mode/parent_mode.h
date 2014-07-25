1784eecd8db400b7e8d72e4073bff03ba6dc4b01
qeesung <1245712564@qq.com>
2014-07-25 21:40:38 +0800

    add tree_node struct and ptree struct

---
+++ Tree/diff_tree/parent_mode/parent_mode.h
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  parent_mode.h
+ *
+ *    Description:  this is parent mode to show a tree
+ *
+ *        Version:  1.0
+ *        Created:  2014年07月25日 21时36分46秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  qeesung (), 1245712564@qq.com
+ *   Organization:  Stay hangry , Stay foolish
+ *
+ * =====================================================================================
+ */
+
+#ifndef _PARENT_MODE_H
+#define _PARENT_MODE_H
+#define MAX_TREE_SIZE 100
+struct tree_node
+{
+    char data[2];
+    int parent;
+};
+typedef struct tree_node * Tree_node;
+
+struct ptree
+{
+    struct tree_node nodes[MAX_TREE_SIZE];
+    int number;
+}
+typedef struct ptree * Ptree;
+#endif
