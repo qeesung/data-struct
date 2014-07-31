aeb8ef4c8eb749f7a40f3158a3b5ad794eb7df6a
qeesung <1245712564@qq.com>
2014-07-31 19:28:23 +0800

    fix Create_Hufftree() function

--- Tree/huffman_tree/huffuman_tree.c
+++ Tree/huffman_tree/huffuman_tree.c
@@ -18,6 +18,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include"huffuman_tree.h"
 #include<limits.h>
 
@@ -57,11 +58,12 @@ Get_Min (Huff_tree my_tree , int index )
     unsigned int weight = UINT_MAX;
     int k=0;
     int min_index;
-    for(k=0;k<index;k++)
+    for(k=1;k<index+1;k++)
     {
         if(my_tree[k].parent==0 && my_tree[k].weight < weight)
         {
             min_index = k;
+            weight = my_tree[k].weight;
         }
     }
     my_tree[min_index].parent=1;
@@ -83,7 +85,7 @@ Select (Huff_tree my_tree , int last_index , int * index1 , int * index2 )
     int temp;
     *index1 = Get_Min(my_tree , last_index);
     *index2 = Get_Min(my_tree , last_index);
-    if(*indx1 < *index2)
+    if(*index1 > *index2)
     {
         temp = *index2;
         *index2=*index1;
@@ -101,67 +103,96 @@ Select (Huff_tree my_tree , int last_index , int * index1 , int * index2 )
  * =====================================================================================
  */
     void
-Create_Hufftree ( Huff_tree my_tree , Huff_code my_code , int * weight_array , int char_number )
+Create_Hufftree ( Huff_tree *my_tree , Huff_code* my_code , int * weight_array , int char_number )
 {
+
    
-    int node_number = 2*char_number -1;
-    int k=0;
-    int min_index1;
-    int min_index2;
-    char code_buf[char_number];
-    int start_index;
-    int parent_temp;
-    int child_temp;
+   unsigned int node_number = 2*char_number -1;
+   unsigned int k=0;
+   unsigned int min_index1;
+   unsigned int min_index2;
+   char * code_buf;
+   unsigned int start_index;
+   unsigned int parent_temp;
+   unsigned int child_temp;
     Huff_tree temp;
-    my_tree	= malloc ( sizeof(struct huff_tree) *(node_number+1) );
-    if ( my_tree==NULL ) {
+    *my_tree	= malloc ( sizeof(struct huff_tree) *(node_number+1) );
+    if ( *my_tree==NULL ) {
         fprintf ( stderr, "\ndynamic memory allocation failed\n" );
         exit (EXIT_FAILURE);
     }
-    temp = my_tree;
+    temp = *my_tree;
     /*  将树初始化 */
     for(k=1;k<char_number+1;k++)
     {
-        temp[k]={weight_array[k-1], 0 ,0,0};
+        temp[k].weight=weight_array[k-1];
+        temp[k].parent =0;
+        temp[k].leftchild =0;
+        temp[k].rightchild =0;
     }
-    for(;k<node_numberi+1;k++)
+    for(;k<node_number+1;k++)
     {
-        temp[k]={0,0,0,0};
+        temp[k].weight=0;
+        temp[k].parent =0;
+        temp[k].leftchild =0;
+        temp[k].rightchild =0;
     }
     /* 现在开始建立树 */
     for(k=char_number+1;k<node_number+1;k++)
     {
-        Select(my_tree , k-1 , &min_index1, &min_index2);
-        my_tree[min_index1].panrent = k ;
-        my_tree[min_index2].panrent = k ;
-        my_tree[k].leftchild = min_index1;
-        my_tree[k].rightchild = min_index2;
-        my_tree[k].weight = my_tree[min_index1].weight + my_tree[min_index2].weight;
+        Select((*my_tree) , k-1 , &min_index1, &min_index2);
+        printf("%d ----> %d\n", min_index1, min_index2);
+        (*my_tree)[min_index1].parent = k ;
+        (*my_tree)[min_index2].parent = k ;
+        (*my_tree)[k].leftchild = min_index1;
+        (*my_tree)[k].rightchild = min_index2;
+        (*my_tree)[k].weight = (*my_tree)[min_index1].weight + (*my_tree)[min_index2].weight;
     }
+    for(k=1;k<node_number+1;k++)
+    {
+        printf("#%d-->weight:%d\t parent:%d\t leftchild:%d\t rightchild:%d\n",k,(*my_tree)[k].weight,\
+            (*my_tree)[k].parent,(*my_tree)[k].leftchild,(*my_tree)[k].rightchild);
+    }
+
+    *my_code	= (Huff_code)malloc ( sizeof(char *) * (char_number+1) );
+    if ( *my_code==NULL ) {
+        fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+        exit (EXIT_FAILURE);
+    }
+    
+    code_buf	= malloc ( sizeof(char) * char_number );
+    if ( code_buf==NULL ) {
+        fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+        exit (EXIT_FAILURE);
+    }
+    code_buf[char_number-1]='\0';
     /*  从叶子向根求huffuman编码 */
     for(k=1;k<char_number+1;k++)
     {
         start_index = char_number-1;
-        code_buf[start_index]='\0';
-        parent_temp  = my_tree[k].parent;
+        parent_temp  = (*my_tree)[k].parent;
         child_temp = k;
+        fflush(stdout);
         while(parent_temp!=0)
         {
-            if(my_tree[parent_temp].leftchild == child_temp )
-                code_buf[--start_idnex]='0';
-            if(my_tree[parent_temp].rightchild == child_temp )
+            if((*my_tree)[parent_temp].leftchild == child_temp )
+            {
+                code_buf[--start_index]='0';
+            }
+            else
+            {
                 code_buf[--start_index]='1';
+            }
             child_temp = parent_temp;
-            parent_temp = my_tree[parent_temp].parent;
+            parent_temp = (*my_tree)[parent_temp].parent;
         }
 
-
-        my_code[k]	= malloc ( sizeof(char) *(char_number - start_index)  );
-        if ( my_code[k]==NULL ) {
+        (*my_code)[k]	= malloc ( sizeof(char) *(char_number - start_index)  );
+        if ( (*my_code)[k]==NULL ) {
             fprintf ( stderr, "\ndynamic memory allocation failed\n" );
             exit (EXIT_FAILURE);
         }
-        strcpy(my_code[k], &code_buf[start_index]);
+        strcpy((*my_code)[k], &code_buf[start_index]);
     }
     return ;
 
--- Tree/huffman_tree/huffuman_tree.h
+++ Tree/huffman_tree/huffuman_tree.h
@@ -23,9 +23,10 @@ struct huff_tree
     unsigned int weight;
     unsigned int parent , leftchild , rightchild;
 };
-
 typedef struct huff_tree * Huff_tree;
-int Get_Min(huff_tree my_tree , int index);//返回在index之前的最小的一个数的序号
+typedef char * * Huff_code;
+int * Random_Weight(void);// 随机生成5个字母的权重随机数
+int Get_Min(Huff_tree my_tree , int index);//返回在index之前的最小的一个数的序号
 void Select(Huff_tree my_tree , int last_index, int * index1 ,int * index2);
-
+void Create_Hufftree(Huff_tree * my_tree ,Huff_code * my_code, int * weight_array, int char_number);
 #endif
