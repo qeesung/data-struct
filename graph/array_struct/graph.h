5a1fa0ea0debab6d1769a2f1c4fa4637ac01efc1
qeesung <1245712564@qq.com>
2014-08-03 14:02:38 +0800

    update graph header file again

--- graph/array_struct/graph.h
+++ graph/array_struct/graph.h
@@ -19,10 +19,11 @@
 #ifndef _GRAPH_H
 #define _GRAPH_H
 #define MAX_GRAPH_SIZE 20
+typedef char Vertex_name[20];// 一个点的最长名字就是20
 typedef enum {DG , DN , UDG , UDN} Graph_kind;
 struct graph
 {
-    char * vertex_name[MAX_GRAPH_SIZE];//用来存储每个顶点的信息 现在用啦存储顶点信息
+    Vertex_name vertex_name[MAX_GRAPH_SIZE];//用来存储每个顶点的信息 现在用啦存储顶点信息
     int arcs[MAX_GRAPH_SIZE][MAX_GRAPH_SIZE];//另接矩阵
     int vertex_number ;
     int arcs_number;
