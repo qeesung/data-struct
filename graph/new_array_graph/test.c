e1b93bdec9c259b545e307c750a0a931b33a27bd
qeesung <1245712564@qq.com>
2014-08-05 21:24:55 +0800

    update the test file

--- graph/new_array_graph/test.c
+++ graph/new_array_graph/test.c
@@ -30,10 +30,56 @@
     int
 main ( int argc, char *argv[] )
 {
+    int k=0;
     Graph my_graph;
+    Graph my_graph1;
     my_graph = Init_Graph();
+    my_graph1 = Init_Graph();
     Create_Graph(my_graph , "./udg_graph.info");
+    Create_Graph(my_graph1 , "./udg_graph.info");
     Print_Graph(my_graph);
+    printf("*******Get vertex**********\n");
+    printf("the v3 name  is :%s\n",Get_Vertex(my_graph , 2));
+    printf("*******Put vertex**********\n");
+    Put_Vertex(my_graph , "V3" ,"XXOO");
+    printf("the v3 name after changed is :%s\n",Get_Vertex(my_graph , 2));
+    Put_Vertex(my_graph , "XXOO" ,"V3");
+    printf("******First Adjacent*********\n");
+    printf("The V1 first adjacent is :%s\n",Get_Vertex(my_graph,First_Adjacent(my_graph , "V1")));
+    printf("******Next Adjacent*********\n");
+    printf("and next adjacent is :");
+    k = Next_Adjacent(my_graph , "V1",Get_Vertex(my_graph,First_Adjacent(my_graph , "V1")));
+    while(k!=-1)
+    {
+        printf("%s\t",Get_Vertex(my_graph,k));
+        k= Next_Adjacent(my_graph , "V1", Get_Vertex(my_graph , k));
+    }
+    printf("\n");
+    printf("****** Insert_Vertex*********\n");
+    Insert_Vertex(my_graph , "QQ");
+    Print_Graph(my_graph);
+    printf("****** delete_Vertex*********\n");
+    Delete_Vertex(my_graph , "V2");
+    Print_Graph(my_graph);
+    printf("********Insert Arcs*********\n");
+    Delete_Vertex(my_graph , "QQ");
+    Insert_Vertex(my_graph , "V2");
+    Insert_Arcs(my_graph , "V1","V2");
+    Insert_Arcs(my_graph , "V3","V2");
+    Insert_Arcs(my_graph , "V5","V2");
+    Print_Graph(my_graph);
+    printf("********Delete Arcs*********\n");
+    Delete_Arcs(my_graph , "V1", "V2");
+    Print_Graph(my_graph);
+    printf("craete a new graph---------->\n");
+    Print_Graph(my_graph1);
+    system("sleep 5");
+    printf("************DFS_TRAVERse**********\n");
+    DFS_Traverse(my_graph1 , Visit);
+    system("sleep 5");
+    printf("***********BFS_TRAVERE*************\n");
+    BFS_Traverse(my_graph1 , Visit);
+    
     free(my_graph);
     my_graph=NULL;
     return EXIT_SUCCESS;
