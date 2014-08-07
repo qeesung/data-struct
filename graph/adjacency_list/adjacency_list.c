7d95aaaf2c43c9f8f94ee15abbf525e0131229f8
qeesung <1245712564@qq.com>
2014-08-07 19:11:06 +0800

    add Create_Graph() function

--- graph/adjacency_list/adjacency_list.c
+++ graph/adjacency_list/adjacency_list.c
@@ -89,3 +89,160 @@ Init_Graph (  )
     new_graph->arcs_numebr =0;
     return new_graph;
 }		/* -----  end of function Init_Graph  ----- */
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Craete_Graph
+ *  Description:  由数据文件来创建一个图, 出错返回-1
+ * =====================================================================================
+ */
+    int
+Craete_Graph ( Graph my_graph  , char * filename )
+{
+    List_node temp;
+    List_node new_node;
+    int index1 , index2;
+    char buf[20];
+    char buf_aim[20];
+    int weight;
+    int length=0;
+    FILE * myfile;
+    int k=0;
+    if(init_error(my_graph))
+        return -1;
+    if(filename == NULL)
+        return -1;
+    /*  打开文件 */
+    if((myfile = fopen(filename , "r"))==NULL)
+    {
+        fprintf(stderr, "\nopen the file stream failed\n");
+        return -1;
+    }
+    /*  读取点的个数 */
+    if(fgets(buf , 20 , myfile)==NULL)
+    {
+        fprintf(stderr, "\n fgets error\n");
+        return -1;
+    }
+    my_graph->vertex_number = atoi(buf);
+    /*  读取弧的个数 */
+    if(fgets(buf , 20 , myfile)==NULL)
+    {
+        fprintf(stderr, "\n fgets error\n");
+        return -1;
+    }
+    my_graph->arcs_numer = atoi(buf);
+    /*  读取图的类型 */
+    if(fgets(buf , 20 , myfile)==NULL)
+    {
+        fprintf(stderr, "\n fgets error\n");
+        return -1;
+    }
+    if(strcmp(buf , "DG\n")==0)
+        my_graph->kind = DG;
+    else
+    {
+        if(strcmp(buf , "DN\n")==0)
+            my_graph->kind = DN;
+        else
+        {
+            if(strcmp(buf , "UDN\n")==0)
+                my_graph->kind = UDN;
+            else
+            {
+                if(strcmp(buf , "UDG\n")==0)
+                    my_graph->kind = UDG;
+                else
+                {
+                    fprintf(stderr, "\n wrong graph kind\n");
+                    return -1;
+                }
+            }
+        }
+    }
+    /*  获取点的名字 */
+    for(k=0;k<my_graph->vertex_number ; k++)
+    {
+        if(fgets(buf , 20 , myfile)==NULL)
+        {
+            fprintf(stderr, "\nfgets error\n");
+            return -1;
+        }
+        strcpy(my_graph->nodes[k].vertex_name , buf);
+    }
+    /*  获取弧的信息 */
+    for(k=0;k<my_graph->arcs_number ; k++)
+    {
+        /* 下面开始处理图 */
+        if(my_graph->kind == DG || my_graph->kind == UDG)
+        {
+            fscanf(myfile , "%s%s",buf , buf_aim);
+            length = strlen(buf_aim);
+            buf_aim[length-1]='\0';
+            index1 = Locate_Vertex(my_graph , buf);
+            index2 = Locate_Vertex(my_graph , buf_aim);
+            if(index1 ==-1 || index2==-1)
+                return -1;
+
+            new_node	= malloc ( sizeof(struct list_node) );
+            if ( new_node==NULL ) {
+                fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+                exit (EXIT_FAILURE);
+            }
+            new_node->index=index2;
+            temp = my_graph->nodes[index1].adj_list->next;
+            my_graph->nodes[index1].adj_list->next = new_node;
+            my_graph->nodes[index1].adj_list->next->next = temp;
+            if(my_graph->kind == UDG)
+            {
+                new_node	= malloc ( sizeof(struct list_node) );
+                if ( new_node==NULL ) {
+                    fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+                    exit (EXIT_FAILURE);
+                }
+                new_node ->index = index1;
+                temp = my_graph->nodes[index2].adj_list->next;
+                my_graph->nodes[index2].adj_list->next = new_node;
+                my_graph->nodes[index2].adj_list->next->next = temp;
+            }
+        
+        }
+        /*   下面开始处理网 */
+        else
+        {
+            fscanf(myfile , "%s%s %d", buf , buf_aim , &weight);
+            index1 = Locate_Vertex(my_graph,buf);
+            index2 = Locate_vertex(my_graph , buf_aim);
+            if(index1 ==-1 || index2==-1)
+                return -1;
+            
+            new_node	= malloc ( sizeof(struct list_node) );
+            if ( new_node==NULL ) {
+                fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+                exit (EXIT_FAILURE);
+            }
+            new_node->index = index2;
+            new_node ->weight = weight;
+            temp = my_graph->nodes[index1].adj_list->next;
+            my_graph->nodes[index1].adj_list->next=new_node;
+            my_graph->nodes[index1].adj_list->next->next = temp;
+            if(my_graph->kind == UDN)
+            {
+                new_node	= malloc ( sizeof(struct list_node) );
+                if ( new_node==NULL ) {
+                    fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+                    exit (EXIT_FAILURE);
+                }
+                new_node->index = index1;
+                new_node ->weight = weight;
+                temp = my_graph->nodes[index2].adj_list->next;
+                my_graph->nodes[index2].adj_list->next=new_node;
+                my_graph->nodes[index2].adj_list->next->next = temp;
+            }
+
+        }
+    }
+    return 1;
+}		/* -----  end of function Craete_Graph  ----- */
