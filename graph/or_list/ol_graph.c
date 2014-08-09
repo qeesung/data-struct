702fe8557ea1a3750970fc8cf2fd054fef7dccd2
qeesung <1245712564@qq.com>
2014-08-09 22:11:36 +0800

    unfinished ol_graph

--- graph/or_list/ol_graph.c
+++ graph/or_list/ol_graph.c
@@ -105,3 +105,95 @@ Init_Graph (  )
 
 
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Create_Graph
+ *  Description:  由文件来创建一个图
+ * =====================================================================================
+ */
+    void
+Create_Graph ( OL_graph graph , char * filename )
+{
+    /* 现在这个图的类型只创建有向图 */
+    FILE * myfile;
+    int length;
+    char buf1[20];
+    char buf2[20];
+    int number;
+    int weight;
+    int k=0;
+    int index1;
+    int index2;
+    Arcbox new_arc;
+    if(init_error(graph))
+        return ;
+    if(filename == NULL)
+    {
+        fprintf(stderr,"filname can not be empty\n");
+        return;
+    }
+    /*打开文件 */
+    if((myfile = fopen(filename , "r"))==NULL)
+    {
+        fprintf(stderr, "\n open the file faield\n");
+        return ;
+    }
+    /*  获得点的个数 */
+    fscanf(myfile , "%d", &number);
+    graph->vertex_number = number;
+    /* 获取弧的个数 */
+    fscanf(myfile , "%d", &number);
+    graph->arcs_number = number;
+    /*  获取图的类型 */
+    fscanf(myfile , "%s", buf1);
+    if(strcmp(buf1 , "DG")==0)
+        graph->kind = DG;
+    else
+    {
+        if(strcmp(buf1, "DN")==0)
+            graph->kind = DN;
+        else
+        {
+            fprintf(stderr, "unknown graph kind\n");
+            return;
+        }
+    }
+    /* 获取点的名字信息 */
+    for(k=0;k<graph->vertex_number;k++)
+    {
+        fscanf(myfile , "%s%s", buf1);
+        strcpy(graph->nodes[k].vertex_name , buf1);
+    }
+    /*  获取弧的信息 */
+    for(k=0;k<graph->arcs_number;k++)
+    {
+        if(graph->kind == DG)
+        {
+            fscanf(myfile , "%s%s", buf1 , buf2);
+        }
+        else
+        {
+            fscanf(myfile , "%s%s %d", buf1 , buf2, &weight);
+        }
+        index1 = Locate_Vertex(graph , buf1);
+        index2 = Locate_Vertex(graph , buf2);
+        if(index1 == -1 || index2 == -1)
+            return ;
+        
+        new_arc	= malloc ( sizeof(struct arcbox) );
+        if ( new_arc==NULL ) {
+            fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+            exit (EXIT_FAILURE);
+        }
+
+        new_arc->next_head = graph->nodes[index2].first_in;
+        new_arc->next_tail = graph->nodes[index1].first_out;
+        new_arc->head = index2;
+        new_arc->tail = index1;
+        if(graph->kind == DN)
+            new_arc->weight = weight;
+    }
+
+
+
+}		/* -----  end of function Create_Graph  ----- */
--- graph/or_list/ol_graph.h
+++ graph/or_list/ol_graph.h
@@ -19,10 +19,11 @@
 #ifndef _OL_GRAPH_H
 #define _OL_GRAPH_H
 #define MAX_GRAPH_SIZE 20
+typedef enum {DG , DN} Graph_kind;
 struct arcbox
 {
-    int headvex;
-    int tailvex;
+    int head;
+    int tail;
     struct arcbox * next_head;
     struct arcbox * next_tail;
     int weight;
@@ -34,7 +35,7 @@ struct vertex
 {
     Vertex_name vertex_name;
     struct arcbox * first_in;
-    struct arcbox * firstout;
+    struct arcbox * first_out;
 } ;  
 typedef struct vertex Vertex;
 
@@ -43,14 +44,16 @@ struct ol_graph
     struct vertex nodes[MAX_GRAPH_SIZE];
     int vertex_number;
     int arcs_number;
-    int graph_kind;
+    Graph_kind graph_kind;
 };
 typedef struct ol_graph * OL_graph;
 
 int init_error(OL_graph);
 int Locate_Ver(OL_graph , Vertex_name name);
 OL_graph Init_Graph();
-
+void Create_Graph(OL_graph graph , char * filename);
+void Destory_Graph(OL_graph);
+void Print_Graph(OL_graph);
 
 
 #endif
