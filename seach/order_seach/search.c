e7e6da79645c898e46ad44d4b34b9e920a833221
qeesung <1245712564@qq.com>
2014-09-02 13:18:35 +0800

    完成顺序查找的C文件，添加相应的函数
        初始化函数:Init_St()
        查找函数:Search()
        销毁函数:Destory()
        遍历函数:Traverse()

--- seach/order_seach/search.c
+++ seach/order_seach/search.c
@@ -21,6 +21,29 @@
 #include "search.h"
 
 
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Init_ST
+ *  Description:  初始化查找表
+ * =====================================================================================
+ */
+    ST
+Init_ST ( void )
+{
+    ST new_st;
+
+    new_st	= malloc ( sizeof(struct search_table) );
+    if ( new_st==NULL ) {
+        fprintf ( stderr, "\ndynamic memory allocation failed\n" );
+        exit (EXIT_FAILURE);
+    }
+    new_st->ele= NULL;
+    return search_table;
+
+}		/* -----  end of function Init_ST  ----- */
+
+
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  Create
@@ -30,6 +53,11 @@
     void
 Create ( ST my_st , char * filename)
 {
+    if(my_st == NULL)
+    {
+        fprintf(stderr,"The search table have not init\n");
+        return;
+    }
     if(filename == NULL)
     {
         fprintf(stderr,"can not find the file!\n");
@@ -71,6 +99,64 @@ Create ( ST my_st , char * filename)
     return ;
 
 }		/* -----  end of function Create  ----- */
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Destory
+ *  Description:  摧毁一个表
+ * =====================================================================================
+ */
+    void
+Destory (ST my_st)
+{
+    if(my_st == NULL)
+    return;
+    free(my_st->ele);
+    my_st->ele = NULL;
+    free(my_st);
+    my_st = NULL;
+    return;
+}		/* -----  end of function Destory  ----- */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Search
+ *  Description:  查找一个元素
+ * =====================================================================================
+ */
+    int
+Search (ST my_st , int key )
+{
+    if(my_st == NULL)
+        return -1;
+    int k;
+    my_st->ele[0]=key;
+    for(k=my_st->length ; key!=my_st->ele[k];k--);
+    return k;
+}		/* -----  end of function Search  ----- */
+
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  Traverse
+ *  Description:  遍历查找表
+ * =====================================================================================
+ */
+    void
+Traverse ( ST my_st )
+{
+    if(my_st == NULL)
+        return;
+    int k=0;
+    for(k=1;k<my_st->length ;k++)
+    {
+        printf("#%d>>>%d\t" ,k , my_st->ele[k]);
+    }
+    return ;
+}		/* -----  end of function Traverse  ----- */
 
 
 
